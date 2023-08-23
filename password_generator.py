b5ab2437f7fb01b7156b5f86f2791c12f39a6955
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2023-08-23 11:32:33 -0500

    perf: changed code to make it better
    Converted code into functions, plus, I made sure that the code works better by using each character possible to generate the password.

--- password_generator.py
+++ password_generator.py
@@ -11,38 +11,36 @@
 import string
 import random
 
-length = int(input("¡Vamos a generar una contraseña! Selecciona una longitud de caracteres entre 8 y 16:\n>"))
+def create_password(length, uppercase=True, numbers=True, punctuations=True):
+    characters = string.ascii_lowercase
+    if uppercase:
+        characters += string.ascii_uppercase
+    if numbers:
+        characters += string.digits
+    if punctuations:
+        characters += string.punctuation
 
-uppercase = input("¿Con o sin letras mayúsculas? 'y' para sí, 'n' para no:\n>").lower()
-
-numbers = input("¿Con o sin números? 'y' para sí, 'n' para no:\n>").lower()
-
-punctuations = input("¿Con o sin símbolos? 'y' para sí, 'n' para no:\n>").lower()
-
-password = ''
-parameters = ['length']
-
-if uppercase == 'y':
-    parameters.append('uppercase')
-
-if numbers == 'y':
-    parameters.append('numbers')
-
-if punctuations == 'y':
-    parameters.append('punctuations')
-
-print(parameters)
-
-for character in range(length):
-    current_parameter = random.choice(parameters)
+    if len(characters) == 0:
+        raise ValueError("Debes seleccionar al menos un tipo de carácter para generar la contraseña.")
     
-    if current_parameter == 'uppercase':
-        password += random.choice(string.ascii_uppercase)
-    elif current_parameter == 'numbers':
-        password += random.choice(string.digits)
-    elif current_parameter == 'punctuations':
-        password += random.choice(string.punctuation)
-    else:
-        password += random.choice(string.ascii_lowercase)
+    password = ''.join([random.choice(characters) for _ in range(length)])
+    return password
 
-print(password)
+def main():
+    try:
+        length = int(input("¡Vamos a generar una contraseña! Selecciona una longitud de caracteres entre 8 y 16:\n>"))
+
+        if length < 8 or length > 16:
+            return print("La longitud debe ser entre 8 y 16.")
+        
+        uppercase = input("¿Con o sin letras mayúsculas? 'y' para sí, 'n' para no:\n>").lower()
+        numbers = input("¿Con o sin números? 'y' para sí, 'n' para no:\n>").lower()
+        punctuations = input("¿Con o sin símbolos? 'y' para sí, 'n' para no:\n>").lower()
+
+        new_password = create_password(length, uppercase == 'y', numbers == 'y', punctuations == 'y')
+        return print(f"Contraseña generada: {new_password}")
+    except ValueError as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
